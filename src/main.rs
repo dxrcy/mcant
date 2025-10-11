a141fa4ebae18a21cdd817fa9d893e8d7a8dc6b1
darcy <acednes@gmail.com>
2025-10-11 15:08:34 +1100

    feat: parse rules from file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::{fmt, fs, iter::Peekable};
+
 use mcrs::{Block, Coordinate};
 
 #[derive(Debug)]
 struct Rule {
-    from_block: &'static [Block],
-    from_state: &'static [State],
+    from_block: Vec<Block>,
+    from_state: Vec<State>,
     to_block: Block,
     to_facing: Rotation,
     to_state: State,
@@ -59,12 +61,7 @@ impl Rotation {
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
-enum State {
-    Searching,
-    Up,
-    Out,
-}
+type State = String;
 
 #[derive(Debug)]
 struct Ant {
@@ -80,37 +77,16 @@ impl Ant {
     }
 }
 
-fn main() -> Result<(), mcrs::Error> {
-    let rules = [
-        Rule {
-            from_block: &[Block::AIR],
-            from_state: &[State::Searching],
-            to_block: Block::AIR,
-            to_facing: Rotation::NegY,
-            to_state: State::Searching,
-        },
-        Rule {
-            from_block: &[Block::DIRT, Block::GRASS],
-            from_state: &[State::Searching],
-            to_block: Block::STONE,
-            to_facing: Rotation::PosY,
-            to_state: State::Up,
-        },
-        Rule {
-            from_block: &[Block::AIR],
-            from_state: &[State::Up],
-            to_block: Block::STONE,
-            to_facing: Rotation::PosX,
-            to_state: State::Out,
-        },
-        Rule {
-            from_block: &[Block::AIR],
-            from_state: &[State::Out],
-            to_block: Block::STONE,
-            to_facing: Rotation::PosY,
-            to_state: State::Up,
-        },
-    ];
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let filepath = "./rules";
+    let text = fs::read_to_string(filepath)?;
+
+    let mut rules = Vec::new();
+
+    let mut parser = Parser::new(&text);
+    while let Some(rule) = parser.next_rule()? {
+        rules.push(rule);
+    }
 
     let mut mc = mcrs::Connection::new()?;
 
@@ -119,7 +95,7 @@ fn main() -> Result<(), mcrs::Error> {
     let mut ant = Ant {
         position: player,
         facing: Rotation::PosX,
-        state: State::Searching,
+        state: "initial".to_string(),
     };
 
     loop {
@@ -143,7 +119,7 @@ fn main() -> Result<(), mcrs::Error> {
         );
 
         mc.set_block(ant.position, rule.to_block)?;
-        ant.state = rule.to_state;
+        ant.state = rule.to_state.clone();
         ant.facing = rule.to_facing;
         ant.move_forward();
 
@@ -151,6 +127,329 @@ fn main() -> Result<(), mcrs::Error> {
     }
 
     Ok(())
+}
+
+struct Parser<'a> {
+    tokens: Peekable<Tokens<'a>>,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(text: &'a str) -> Self {
+        Self {
+            tokens: Tokens::new(text).peekable(),
+        }
+    }
+
+    pub fn next_rule(&mut self) -> Result<Option<Rule>, String> {
+        if self.tokens.peek().is_none() {
+            return Ok(None);
+        }
+
+        let mut from_state = Vec::new();
+        for item in ListParser::new(&mut self.tokens) {
+            let item = item?;
+            from_state.push(item.to_string());
+        }
+        self.expect_list_end(TokenKind::Comma)?;
+
+        let mut from_block = Vec::new();
+        for item in ListParser::new(&mut self.tokens) {
+            let item = item?;
+            let Some(block) = Self::parse_block(item) else {
+                return Err(format!("unknown block `{}`", item));
+            };
+            from_block.push(block);
+        }
+        self.expect_list_end(TokenKind::Comma)?;
+
+        let mut from_facing = Vec::new();
+        for item in ListParser::new(&mut self.tokens) {
+            let item = item?;
+            let Some(facing) = Self::parse_rotation(item) else {
+                return Err(format!("unknown rotation `{}`", item));
+            };
+            from_facing.push(facing);
+        }
+        self.expect_list_end(TokenKind::Arrow)?;
+
+        let to_state = self.expect_ident()?.to_string();
+        self.expect_list_end(TokenKind::Comma)?;
+
+        let to_block_str = self.expect_ident()?;
+        let Some(to_block) = Self::parse_block(to_block_str) else {
+            return Err(format!("unknown block `{}`", to_block_str));
+        };
+        self.expect_list_end(TokenKind::Comma)?;
+
+        let to_facing_str = self.expect_ident()?;
+        let Some(to_facing) = Self::parse_rotation(to_facing_str) else {
+            return Err(format!("unknown rotation `{}`", to_facing_str));
+        };
+        self.expect_list_end(TokenKind::Semicolon)?;
+
+        Ok(Some(Rule {
+            from_state,
+            from_block,
+            // from_facing,
+            to_state,
+            to_block,
+            to_facing,
+        }))
+    }
+
+    fn expect_ident(&mut self) -> Result<&'a str, String> {
+        let Some(next) = self.tokens.next() else {
+            return Err(format!("expected ident, found eof"));
+        };
+        if next.kind != TokenKind::Ident {
+            return Err(format!("expected ident, found {}", next.kind));
+        }
+        Ok(next.string)
+    }
+
+    fn expect_list_end(&mut self, end: TokenKind) -> Result<(), String> {
+        let Some(next) = self.tokens.next() else {
+            return Err(format!(
+                "expected {} or {}, found eof",
+                end,
+                TokenKind::Slash,
+            ));
+        };
+        if next.kind != end {
+            return Err(format!(
+                "expected {} or {}, found {}",
+                end,
+                TokenKind::Slash,
+                next.kind
+            ));
+        }
+        Ok(())
+    }
+
+    fn parse_block(string: &str) -> Option<Block> {
+        for (name, block) in mcrs::BLOCKS {
+            if name.eq_ignore_ascii_case(string) {
+                return Some(block);
+            }
+        }
+        return None;
+    }
+
+    fn parse_rotation(string: &str) -> Option<Rotation> {
+        const ROTATIONS: &[(&str, Rotation)] = &[
+            ("east", Rotation::PosX),
+            ("west", Rotation::NegX),
+            ("up", Rotation::PosY),
+            ("down", Rotation::NegY),
+            ("south", Rotation::PosZ),
+            ("north", Rotation::NegZ),
+        ];
+
+        for (name, rotation) in ROTATIONS {
+            if name.eq_ignore_ascii_case(string) {
+                return Some(*rotation);
+            }
+        }
+        return None;
+    }
+}
+
+struct ListParser<'r, 'a> {
+    tokens: &'r mut Peekable<Tokens<'a>>,
+    first: bool,
+    end: bool,
+}
+
+impl<'r, 'a> ListParser<'r, 'a> {
+    pub fn new(tokens: &'r mut Peekable<Tokens<'a>>) -> Self {
+        Self {
+            tokens,
+            first: true,
+            end: false,
+        }
+    }
+}
+
+impl<'r, 'a> Iterator for ListParser<'r, 'a> {
+    type Item = Result<&'a str, String>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.end {
+            return None;
+        }
+
+        let Some(peek) = self.tokens.peek() else {
+            return Some(Err(format!("expected token, found eof")));
+        };
+        if peek.kind != TokenKind::Ident {
+            if self.first {
+                return None;
+            }
+            return Some(Err(format!(
+                "expected {} or {}, found {}",
+                TokenKind::Ident,
+                TokenKind::Comma,
+                peek.kind,
+            )));
+        }
+
+        let next = self.tokens.next().unwrap();
+
+        if self.tokens.peek()?.kind == TokenKind::Slash {
+            self.first = false;
+            _ = self.tokens.next().unwrap();
+        } else {
+            self.end = true;
+        }
+
+        Some(Ok(next.string))
+    }
+}
+
+#[derive(Debug)]
+struct Token<'a> {
+    string: &'a str,
+    kind: TokenKind,
+}
+
+impl<'a> Token<'a> {
+    pub fn from(string: &'a str) -> Self {
+        Self {
+            string,
+            kind: TokenKind::from(string),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum TokenKind {
+    Slash,
+    Comma,
+    Arrow,
+    Semicolon,
+    Ident,
+}
+
+impl TokenKind {
+    pub fn from(string: &str) -> Self {
+        match string {
+            "/" => Self::Slash,
+            "," => Self::Comma,
+            "->" => Self::Arrow,
+            ";" => Self::Semicolon,
+            _ => Self::Ident,
+        }
+    }
+}
+
+impl fmt::Display for TokenKind {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Slash => write!(f, "`/`"),
+            Self::Comma => write!(f, "`,`"),
+            Self::Arrow => write!(f, "`->`"),
+            Self::Semicolon => write!(f, "`;`"),
+            Self::Ident => write!(f, "<identifier>"),
+        }
+    }
+}
+
+struct Tokens<'a> {
+    text: &'a str,
+    cursor: usize,
+}
+
+impl<'a> Tokens<'a> {
+    pub fn new(text: &'a str) -> Self {
+        Self { text, cursor: 0 }
+    }
+
+    fn is_end(&self) -> bool {
+        self.cursor >= self.text.len()
+    }
+
+    fn peek_char(&self) -> Option<char> {
+        let mut chars = self.text[self.cursor..].chars();
+        let ch = chars.next()?;
+        Some(ch)
+    }
+
+    fn next_char(&mut self) -> Option<char> {
+        let mut chars = self.text[self.cursor..].chars();
+        let ch = chars.next()?;
+        self.cursor += ch.len_utf8();
+        Some(ch)
+    }
+
+    fn advance_until_nonwhitespace(&mut self) {
+        while let Some(ch) = self.peek_char() {
+            if !Self::is_whitespace(ch) {
+                break;
+            }
+            _ = self.next_char();
+        }
+    }
+
+    fn try_atomic(&mut self) -> Option<&'a str> {
+        assert!(!self.is_end());
+
+        let first = self.peek_char().unwrap();
+        assert!(!Self::is_whitespace(first));
+        if !Self::is_atomic(first) {
+            return None;
+        }
+
+        let start = self.cursor;
+        _ = self.next_char().unwrap();
+
+        Some(&self.text[start..self.cursor])
+    }
+
+    fn expect_combination(&mut self) -> &'a str {
+        assert!(!self.is_end());
+
+        let start = self.cursor;
+        let first = self.next_char().unwrap();
+        assert!(!Self::is_whitespace(first));
+        let is_symbol = Self::is_symbol(first);
+
+        while let Some(ch) = self.peek_char() {
+            if Self::is_whitespace(ch) || Self::is_atomic(ch) || Self::is_symbol(ch) != is_symbol {
+                break;
+            }
+            _ = self.next_char().unwrap();
+        }
+
+        &self.text[start..self.cursor]
+    }
+
+    fn is_whitespace(ch: char) -> bool {
+        ch.is_ascii_whitespace()
+    }
+    fn is_atomic(ch: char) -> bool {
+        matches!(ch, ',' | ';' | '(' | ')' | '[' | ']' | '{' | '}')
+    }
+    fn is_symbol(ch: char) -> bool {
+        matches!(ch, '\x21'..='\x2f' | '\x3a'..='\x40'|'\x5b'..='\x60'|'\x7b'..='\x7e')
+    }
+}
+
+impl<'a> Iterator for Tokens<'a> {
+    type Item = Token<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.advance_until_nonwhitespace();
+
+        if self.is_end() {
+            return None;
+        }
+
+        let string = self
+            .try_atomic()
+            .unwrap_or_else(|| self.expect_combination());
+
+        Some(Token::from(string))
+    }
 }
 
 fn show_ant_indicator(mc: &mut mcrs::Connection, position: Coordinate) -> Result<(), mcrs::Error> {
@@ -187,7 +486,9 @@ fn show_ant_indicator(mc: &mut mcrs::Connection, position: Coordinate) -> Result
 
 fn find_rule<'a>(rules: &'a [Rule], ant: &Ant, block: Block) -> Option<&'a Rule> {
     for rule in rules {
-        if rule.from_block.contains(&block) && rule.from_state.contains(&ant.state) {
+        if (rule.from_block.is_empty() || rule.from_block.contains(&block))
+            && (rule.from_state.is_empty() || rule.from_state.contains(&ant.state))
+        {
             return Some(rule);
         }
     }
